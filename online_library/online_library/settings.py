3abaa3a2fecdec2f0fbeb8d2147e7c70b9a3825c
YvevinnTan <yvevinn1324@gmail.com>
2025-05-13 22:39:23 +0800

    update header

--- online_library/online_library/settings.py
+++ online_library/online_library/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR / 'templates'
+STATIC_PATH = BASE_DIR / 'static'
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    STATIC_PATH,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
