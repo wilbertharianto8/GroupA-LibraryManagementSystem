39b0160b686fff7f9b2d57170524566a09cf79a2
Lu Jie Er <jelu022498@gmail.com>
2025-06-27 11:16:22 +0800

    revert settings to base settings

--- online_library/online_library/settings.py
+++ online_library/online_library/settings.py
@@ -84,20 +84,13 @@ WSGI_APPLICATION = 'online_library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'shared_db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'shared_db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
